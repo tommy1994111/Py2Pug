b46cdd2f1c2d9652682293bd1992eac22939c543
Tommy Lai <tommy1994111@gmail.com>
2017-06-09 02:21:12 +0800

    prototype

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 class Py2Pug:
-    def __init__(self):
+    def __init__(self, doctype='doctype html', indent=2):
         self.html = Html()
-		self.doctype = 'doctype html'
-
+            self.doctype = doctype
+            self.indent = indent
+	
+	def compileToPug(self):
+		pass
 
 class Html:
     def __init__(self):
@@ -12,12 +15,18 @@ class Html:
 
 class Head:
     def __init__(self):
-        self.content = dict()
+        self.content = Tag('head')
+
+        def addContent(self, content):
+            pass
 
 
 class Body:
     def __init__(self):
-        self.content = dict()
+        self.content = Tag('body')
+
+        def addContent(self, content):
+            pass
 
 
 class Tag:
@@ -28,19 +37,27 @@ class Tag:
         self.attributes = attributes
         self.content = content
 
-	def compileClassNames(self):
-		if self.classNames not None:
-			return ''.join(['.{}'.format(className) for className in self.classNames])
-		else:
-			return None
+        def compileClassNames(self):
+            if self.classNames not None:
+                return ''.join(['.{}'.format(className) for className in self.classNames])
+            else:
+                return None
 
-	def compileAttributes(self):
-		if self.attributes not None:
-			return '({})'.format(' '.join(self.attributes))
-		else:
-			return None
-	
-	def __str__(self):
-		return '''{tagName}{classNames}{idName}{attributes}{content}'''.format(self.tagName, self.classNames, self.idName, self.attributes, self.content)
+        def compileAttributes(self):
+            if self.attributes not None:
+                return '({})'.format(' '.join(self.attributes))
+            else:
+                return None
 
-Tag('div', classNames=['ts', 'btn', 'primary'], idName='button', attributes=['@click="doSth()"', ':value=""'], content=Tag('p'))
+        def addContent(self, content):
+            pass
+
+        def __str__(self):
+            return '''{tagName}{classNames}{idName}{attributes}{content}'''.format(self.tagName, self.classNames, self.idName, self.attributes, self.content)
+
+
+pug = Py2Pug()
+pug.html.head.addContent(Tag('script'))
+pug.html.body.addContent(Tag('div', classNames=['ts', 'btn', 'primary'], idName='button', attributes=['@click="doSth()"', ':value=""'], content=Tag('p'))
+                         )
+pug.compileToPug()
